62c1a6ca597f583bddce0f64147297f808033e6a
huyphung1602 <huy.phung.sw@gmail.com>
2018-09-08 16:48:13 +0700

    Add create_communes to ImportJob, add new column population into communes table

--- app/jobs/import_job.rb
+++ app/jobs/import_job.rb
@@ -2,13 +2,15 @@ require 'csv'
 
 class ImportJob < ApplicationJob
 
-  InterStruct = Struct.new(:name, :siren, :form)
+  InterComStruct = Struct.new(:name, :siren, :form)
+  ComStruct = Struct.new(:name, :code_insee, :population)
 
   class << self
     def perform_now(csv)
       csv = generate_to_text(csv)
       header = true
       intercommunalities = []
+      communes = []
 
       CSV.parse(csv)  do |row|
         if header
@@ -18,22 +20,47 @@ class ImportJob < ApplicationJob
 
         array = row.first.split("\;")
 
+        communes << commune_structure(array)
+
         unless intercommunalities.map(&:name).include?(array[2])
           intercommunalities << intercommunality_structure(array)
         end
       end
 
+      create_communes(communes)
       create_intercommunalities(intercommunalities)
     end
 
     private
 
+    def commune_structure(array)
+      ComStruct.new(
+        array[8],
+        array[6],
+        array[9]
+      )
+    end
+
     def intercommunality_structure(array)
-      InterStruct.new(
+      InterComStruct.new(
         array[2],
         array[1],
         array[3].slice(0, 3).downcase
       )
+    end
+
+    def create_communes(communes)
+      communes.each do |commune|
+        create_commune(commune)
+      end
+    end
+
+    def create_commune(commune)
+      Commune.new(
+        name: commune.name,
+        code_insee: commune.code_insee,
+        population: commune.population
+      ).save!
     end
 
     def create_intercommunalities(intercommunalities)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180907110541) do
+ActiveRecord::Schema.define(version: 20180908094307) do
 
   create_table "communes", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180907110541) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.integer  "intercommunality_id"
+    t.integer  "population"
   end
 
   create_table "intercommunalities", force: :cascade do |t|
